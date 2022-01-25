e394d9d809c0b502a59206687c3409618f347e0a
ikarteeva <ikarteeva@yandex.ru>
2022-01-25 10:14:45 +0300

    starting quiz with openedu

--- app/src/main/java/com/example/apptry/MainActivity.java
+++ app/src/main/java/com/example/apptry/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.apptry;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nobut = findViewById(R.id.nobut);
+        nobut.setOnClickListener(view -> Toast.makeText(MainActivity.this, R.string.uncorrect, Toast.LENGTH_LONG).show());
+        Button yesbut = findViewById(R.id.yesbut);
+        yesbut.setOnClickListener(view -> Toast.makeText(MainActivity.this, R.string.correct, Toast.LENGTH_LONG).show());
+
     }
+
+
 }
